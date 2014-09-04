f97f3702f3a6bab4abf14c48fbe07d9230f6f798
gutterwall <gutterwall@gmail.com>
2014-09-04 11:52:41 -0400

    Fixed Artwork on Player, added Seekbar functionality
    Seekbar, Artwork!

--- app/src/main/java/org/wrir/wrirplayer/PlayerControl.java
+++ app/src/main/java/org/wrir/wrirplayer/PlayerControl.java
@@ -1,8 +1,14 @@
 package org.wrir.WrirPlayer;
 
+import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
@@ -14,6 +20,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 
 
 /**
@@ -28,7 +35,7 @@ public class PlayerControl extends Activity {
     private Handler myHandler = new Handler();
     private int forwardTime = 5000;
     private int backwardTime = 5000;
-    private SeekBar seekbar;
+    private SeekBar seekbar = null;
     private ImageButton playButton,pauseButton;
     public static int oneTimeOnly = 0;
     @Override
@@ -40,31 +47,52 @@ public class PlayerControl extends Activity {
         String mp3 = "";
         String title = "";
 
-        Log.w( "WRIR","WRIR: creating PlayerControl");
+        Log.w("WRIR", "WRIR: creating PlayerControl");
         Bundle extras = getIntent().getExtras();
         mp3 = extras.getString("mp3");
         title = extras.getString("title");
-        Uri posterurl = Uri.parse(extras.getString("poster"));
-        Log.w("WRIR","WRIR: " + mp3 + " " + title);
+        String posterurl = extras.getString("poster");
+        Log.w("WRIR", "WRIR: " + mp3 + " " + title);
         Uri mp3url = Uri.parse(mp3);
-        Log.w("WRIR","WRIR: parsed uri");
+        Log.w("WRIR", "WRIR: parsed uri");
 
-        poster = (ImageView)findViewById(R.id.poster);
-        songName = (TextView)findViewById(R.id.textView4);
-        startTimeField =(TextView)findViewById(R.id.textView1);
-        endTimeField =(TextView)findViewById(R.id.textView2);
-        seekbar = (SeekBar)findViewById(R.id.seekBar1);
-        playButton = (ImageButton)findViewById(R.id.imageButton1);
-        pauseButton = (ImageButton)findViewById(R.id.imageButton2);
+        poster = (ImageView) findViewById(R.id.poster);
+        songName = (TextView) findViewById(R.id.textView4);
+        startTimeField = (TextView) findViewById(R.id.textView1);
+        endTimeField = (TextView) findViewById(R.id.textView2);
+        seekbar = (SeekBar) findViewById(R.id.seekBar1);
+        playButton = (ImageButton) findViewById(R.id.imageButton1);
+        pauseButton = (ImageButton) findViewById(R.id.imageButton2);
         songName.setText(title);
-        Log.w("WRIR","WRIR: About to create uri");
-        mediaPlayer = MediaPlayer.create(this,mp3url);
-        Log.w("WRIR","WRIR: Created uri");
-        seekbar.setClickable(false);
+        Log.w("WRIR", "WRIR: About to create uri");
+        mediaPlayer = MediaPlayer.create(this, mp3url);
+        Log.w("WRIR", "WRIR: Created uri");
+        seekbar.setClickable(true);
         pauseButton.setEnabled(false);
-        poster.setImageURI(posterurl);
 
+        new DownloadImageTask((ImageView) findViewById(R.id.poster)).execute(posterurl);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                   progressChanged = progress;
+              }
+
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    //mediaPlayer.pause();
+                }
+
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    //Toast.makeText(SeekbarActivity.this,"seek bar progress:"+progressChanged,
+                    //       Toast.LENGTH_SHORT).show();
+                    //startTime = startTime + progressChanged;
+                    mediaPlayer.seekTo((int) progressChanged);
+                    //mediaPlayer.start();
+                }
+
+        });
     }
+
+
 
     public void play(View view){
         Toast.makeText(getApplicationContext(), "Playing sound",
@@ -108,6 +136,7 @@ public class PlayerControl extends Activity {
             myHandler.postDelayed(this, 100);
         }
     };
+
     public void pause(View view){
         Toast.makeText(getApplicationContext(), "Pausing sound",
                 Toast.LENGTH_SHORT).show();
@@ -148,6 +177,31 @@ public class PlayerControl extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 
 }
